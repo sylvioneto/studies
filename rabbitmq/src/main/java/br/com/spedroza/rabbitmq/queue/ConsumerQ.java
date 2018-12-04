ed09628af8f1347c84e3de8829e0c6008f2656a5
sylvio <sylvio@sylvio-laptop>
2018-12-04 20:20:32 -0200

    task queue and manual ack

--- rabbitmq/src/main/java/br/com/spedroza/rabbitmq/queue/ConsumerQ.java
+++ rabbitmq/src/main/java/br/com/spedroza/rabbitmq/queue/ConsumerQ.java
@@ -1,5 +1,7 @@
 package br.com.spedroza.rabbitmq.queue;
 
+import java.util.Calendar;
+
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
@@ -7,25 +9,30 @@ import com.rabbitmq.client.DeliverCallback;
 
 public class ConsumerQ {
 
+	// queue name
 	private final static String QUEUE_NAME = "test.incoming";
 
 	public static void main(String[] argv) throws Exception {
 
 		// create a connection to the server
 		ConnectionFactory factory = new ConnectionFactory();
-	    factory.setHost("192.168.99.100");
+	    factory.setHost("localhost");
 	    Connection connection = factory.newConnection();
 	    Channel channel = connection.createChannel();
 
-		// queue declare
+		// queue declare - it will create it in case it does not exists
 	    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+	    
 	    System.out.println("Waiting for messages. To exit press CTRL+C");
 		
 	    //deliver call back
 	    DeliverCallback deliverCallback = (consumerTag, delivery) -> {
 	        String message = new String(delivery.getBody(), "UTF-8");
-	        System.out.println(" [x] Received '" + message + "'");
+	        System.out.println(Calendar.getInstance().getTime()+ " - Received - " + message);
 	    };
+	    
+	    // basic consumer using autoAck
 	    channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
 	}
+	
 }
--- rabbitmq/src/main/java/br/com/spedroza/rabbitmq/queue/ProducerQ.java
+++ rabbitmq/src/main/java/br/com/spedroza/rabbitmq/queue/ProducerQ.java
@@ -1,8 +1,12 @@
 package br.com.spedroza.rabbitmq.queue;
 
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+import br.com.spedroza.test.builder.TestMessage;
 
 public class ProducerQ {
 
@@ -12,22 +16,36 @@ public class ProducerQ {
 
 		// create a connection to the server
 		ConnectionFactory factory = new ConnectionFactory();
-		factory.setHost("192.168.99.100");
-		
+		factory.setHost("localhost");
+
 		try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
-		
+
 			// declare will create the queue in case it does not exists
 			System.out.println("Queue declare...");
 			channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-			
-			//send a hello world message
-			System.out.println("Sending message...");
-			
-			// send 10 messages
-			for(int i = 0; i <= 10; i++) {
-				String message = "Hello World! Count "+i;
-				channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-				System.out.println("Message Sent " + message);	
+
+			// send messages
+			System.out.println("Sending message.s..");
+
+			// send 10 text plan messages
+			for (int i = 0; i < 10; i++) {
+				String message = "Message no " + i;
+				channel.basicPublish("", QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
+				System.out.println("Message Sent " + message);
+			}
+
+			// send 5 json messages
+			for (int i = 0; i < 5; i++) {
+				String message = new TestMessage().getJsonMessage().toString();
+				channel.basicPublish("", QUEUE_NAME,
+						new AMQP.BasicProperties.Builder()
+			               .contentType("application/json")
+			               .deliveryMode(2)
+			               .priority(1)
+			               .messageId(String.valueOf(i))
+			               .build(),
+			               message.getBytes());
+				System.out.println("Message Sent " + message);
 			}
 
 		} catch (Exception e) {
