68014598d594bb0fc9c98384f37b587a0c92aa3b
spedroza <spedroza@SPEDROZA-BR.br.oracle.com>
2018-12-04 12:24:05 -0200

    first commit. studying rabbit mq

---
+++ rabbitmq/src/main/java/br/com/spedroza/rabbitmq/queue/ConsumerRabbitMQ.java
@@ -0,0 +1,31 @@
+package br.com.spedroza.rabbitmq.queue;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+public class ConsumerRabbitMQ {
+
+	private final static String QUEUE_NAME = "test.incoming";
+
+	public static void main(String[] argv) throws Exception {
+
+		// create a connection to the server
+		ConnectionFactory factory = new ConnectionFactory();
+	    factory.setHost("192.168.99.100");
+	    Connection connection = factory.newConnection();
+	    Channel channel = connection.createChannel();
+
+		// queue declare
+	    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+	    System.out.println("Waiting for messages. To exit press CTRL+C");
+		
+	    //deliver call back
+	    DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+	        String message = new String(delivery.getBody(), "UTF-8");
+	        System.out.println(" [x] Received '" + message + "'");
+	    };
+	    channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+	}
+}
---
+++ rabbitmq/src/main/java/br/com/spedroza/rabbitmq/queue/ProducerRabbitMQ.java
@@ -0,0 +1,37 @@
+package br.com.spedroza.rabbitmq.queue;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class ProducerRabbitMQ {
+
+	private final static String QUEUE_NAME = "test.incoming";
+
+	public static void main(String[] argv) throws Exception {
+
+		// create a connection to the server
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("192.168.99.100");
+		
+		try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
+		
+			// declare will create the queue in case it does not exists
+			System.out.println("Queue declare...");
+			channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+			
+			//send a hello world message
+			System.out.println("Sending message...");
+			
+			// send 10 messages
+			for(int i = 0; i <= 10; i++) {
+				String message = "Hello World! Count "+i;
+				channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+				System.out.println("Message Sent " + message);	
+			}
+
+		} catch (Exception e) {
+			System.out.println("ProducerRabbitMQ exception: " + e.getMessage());
+		}
+	}
+}
